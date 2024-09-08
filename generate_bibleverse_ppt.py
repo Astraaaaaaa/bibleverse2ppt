19a03fc69d43908fedeebdbe9adbb0e450ab19f2
astraaaaaaa <46011386+Astraaaaaaa@users.noreply.github.com>
2024-09-08 20:46:33 +0800

    Update generate_bibleverse_ppt.py

--- generate_bibleverse_ppt.py
+++ generate_bibleverse_ppt.py
@@ -30,6 +30,7 @@ from selenium.webdriver.support.ui import Select
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
 from webdriver_manager.chrome import ChromeDriverManager
 from webdriver_manager.firefox import GeckoDriverManager
